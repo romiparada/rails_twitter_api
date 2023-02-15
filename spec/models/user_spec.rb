49a67a9f6ca4d39015c18bc94b65d0d5bc6c0a04
Romina <romina@gogrow.dev>
2023-02-15 14:39:05 -0300

    Add user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:user) }
+
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  end
 end
