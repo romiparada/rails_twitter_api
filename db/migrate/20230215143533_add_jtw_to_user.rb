e6a84501a15172f6f0ecad0cb3d3ff2401564a92
Romina <romina@gogrow.dev>
2023-02-17 12:43:21 -0300

    Run rubocop

--- db/migrate/20230215143533_add_jtw_to_user.rb
+++ db/migrate/20230215143533_add_jtw_to_user.rb
@@ -2,7 +2,7 @@
 
 class AddJtwToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :jti, :string, null: false
+    add_column :users, :jti, :string, null: false, default: ''
     add_index :users, :jti, unique: true
   end
 end
