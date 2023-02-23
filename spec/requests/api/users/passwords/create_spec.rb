257fb23153b6c5426aca0de8d861677b90e9e16e
Romina <romina@gogrow.dev>
2023-02-23 13:41:38 -0300

    Fix path in spec

--- spec/requests/api/users/passwords/create_spec.rb
+++ spec/requests/api/users/passwords/create_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'POST /users/password', type: :request do
+RSpec.describe 'POST /api/users/password', type: :request do
   subject { post user_password_path, params:, as: :json }
 
   let(:user) { create(:user) }
