8fe30366a01b364606850dbf83b6e3f16346e5f0
Romina <romina@gogrow.dev>
2023-02-24 17:35:02 -0300

    Add fields required action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,19 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
   respond_to :json
+  before_action :authenticate_user!
+  before_action :require_user_fields, unless: :devise_controller?
+
+  private
+
+  def require_user_fields
+    user = current_user
+    missing_fileds = %i[name birthdate username].select do |field|
+      user.send(field).blank?
+    end
+    return if missing_fileds.blank?
+
+    render json: { errors: "#{missing_fileds.join(', ')} fields are required" },
+           status: :unprocessable_entity
+  end
 end
