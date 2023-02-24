3f297fea7fb3f9a117755699483c0b59601e8e25
Romina <romina@gogrow.dev>
2023-02-24 16:10:05 -0300

    Add fields to user and model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,5 +9,16 @@ FactoryBot.define do
     trait :unconfirmed do
       confirmed_at { nil }
     end
+
+    trait :full do
+      name { Faker::Internet.password(min_length: 2) }
+      bio { Faker::Lorem.paragraph_by_chars(number: 160) }
+      website do
+        ["http://#{Faker::Internet.domain_word}.com", "http://www.#{Faker::Internet.domain_word}.com",
+         "https://#{Faker::Internet.domain_word}.com", "https://#{Faker::Internet.domain_word}.com"]
+      end
+      birthdate { Faker::Date.birthday(min_age: 18) }
+      username { Faker::Internet.username(specifier: 2..20) }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,36 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    subject { build(:user) }
+    subject { build(:user, :full) }
 
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    context 'email' do
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    end
+
+    context 'password' do
+      it { is_expected.to validate_presence_of(:password) }
+    end
+
+    context 'username' do
+      it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+      it { is_expected.to validate_length_of(:username).is_at_least(2).is_at_most(20) }
+    end
+
+    context 'name' do
+      it { is_expected.to validate_length_of(:name).is_at_least(2) }
+    end
+
+    context 'bio' do
+      it { is_expected.to validate_length_of(:bio).is_at_most(160) }
+    end
+
+    context 'validates website format' do
+      it { is_expected.to_not allow_value(Faker::Internet.domain_name).for(:website) }
+    end
+
+    context 'validates birthday' do
+      it { is_expected.to_not allow_value(Faker::Date.birthday(max_age: 17)).for(:birthdate) }
+    end
   end
 end
