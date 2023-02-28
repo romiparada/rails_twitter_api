47e97a4c4f591d855479f256c10e1f628b0a2feb
Romina <romina@gogrow.dev>
2023-02-28 10:49:56 -0300

    Fix website attribute on user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -15,7 +15,7 @@ FactoryBot.define do
       bio { Faker::Lorem.paragraph_by_chars(number: 160) }
       website do
         ["http://#{Faker::Internet.domain_word}.com", "http://www.#{Faker::Internet.domain_word}.com",
-         "https://#{Faker::Internet.domain_word}.com", "https://#{Faker::Internet.domain_word}.com"]
+         "https://#{Faker::Internet.domain_word}.com", "https://#{Faker::Internet.domain_word}.com"].sample
       end
       birthdate { Faker::Date.birthday(min_age: 18) }
       username { Faker::Internet.username(specifier: 2..20) }
