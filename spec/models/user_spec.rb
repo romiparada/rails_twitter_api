4d0063af0a7f3d5820bc92c9c7447610da8cada8
Romina <romina@gogrow.dev>
2023-03-02 10:33:13 -0300

    Fix user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    subject { build(:user) }
+  subject { build(:user) }
 
+  describe 'validations' do
     context 'email' do
       it { is_expected.to validate_presence_of(:email) }
       it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
@@ -36,8 +36,40 @@ RSpec.describe User, type: :model do
       it { is_expected.to_not allow_value(Faker::Internet.domain_name).for(:website) }
     end
 
-    context 'validates birthday' do
-      it { is_expected.to_not allow_value(Faker::Date.birthday(max_age: 17)).for(:birthdate) }
+    describe '#over_18_years_old' do
+      context 'when user is over 18' do
+        before { subject.birthdate = Faker::Date.birthday(min_age: 18) }
+
+        it 'does not add errors to user' do
+          subject.send(:over_18_years_old)
+          expect(subject.errors[:birthdate]).to be_empty
+        end
+      end
+
+      context 'when user is under 18' do
+        before { subject.birthdate = Faker::Date.birthday(max_age: 17) }
+
+        it 'adds errors to user' do
+          subject.send(:over_18_years_old)
+          expect(subject.errors[:birthdate]).to eq(['User must be at least 18 years old'])
+        end
+      end
+    end
+  end
+
+  describe '#missing_required_fields' do
+    context 'whent the username is complete' do
+      it 'returns an empty array' do
+        expect(subject.missing_required_fields).to be_empty
+      end
+    end
+
+    context 'when the username is incomplete' do
+      subject { build(:user, :without_profile_data) }
+
+      it 'returns the fields name, birthdate and username' do
+        expect(subject.missing_required_fields).to eq(%i[name birthdate username])
+      end
     end
   end
 end
