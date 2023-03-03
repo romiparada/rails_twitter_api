f139ed5e853722e537eb45ffcd6f88fcf1ced198
Romina Parada <38337127+romiparada@users.noreply.github.com>
2023-03-03 13:05:57 -0300

    Add a show profile endpoint (#20)
    * Add more fields to user (#19)
    
    * Add fields to user and model specs
    
    * Add fields required action
    
    * Fix user name in factory
    
    * Fix website attribute on user factory
    
    * Refactor before actions
    
    * Delete full trait from user factory
    
    * Add valid value for website in user spec
    
    * Add trait without profile data to user
    
    * Fix user model spec
    
    * Add a show profile endpoint
    
    * Add skip to require user fields before action to show
    
    * Add field to serializer and json as default in rute
    
    * Fix show spec
    
    * Add PR comments

---
+++ spec/requests/api/user/show_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /api/user', type: :request do
+  subject { get user_path, headers:, as: :json }
+
+  let(:user) { create(:user) }
+  let(:headers) { auth_headers(user) }
+
+  context 'when the valid auth headers are passed' do
+    it 'returns 200 status code' do
+      subject
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns user profile info' do
+      subject
+      json_res = json
+      expect(json_res['name']).to eq(user.name)
+      expect(json_res['bio']).to eq(user.bio)
+      expect(json_res['website']).to eq(user.website)
+      expect(json_res['email']).to eq(user.email)
+      expect(json_res['created_at']).to eq(user.created_at.strftime('%Y-%m-%d %H:%M:%S UTC'))
+      expect(json_res['username']).to eq(user.username)
+    end
+  end
+
+  context 'when invalid auth headers are passed' do
+    context 'when the auth header is not set' do
+      let(:headers) { {} }
+
+      it 'returns 422 status code' do
+        subject
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
+end
